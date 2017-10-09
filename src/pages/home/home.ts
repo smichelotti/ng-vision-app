eef4827a157cc7704653a7183a72987c060c815a
Steve Michelotti <steve.michelotti@gmail.com>
2017-10-09 08:56:17 -0400

    initial code commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Camera, CameraOptions, DestinationType, EncodingType, PictureSourceType } from '@ionic-native/camera';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  private imgPath: string;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private camera: Camera) {
 
+  }
+
+  takePicture() {
+    let options: CameraOptions = {
+      destinationType: DestinationType.FILE_URL,
+      sourceType: PictureSourceType.CAMERA,
+      encodingType: EncodingType.PNG,
+      targetHeight: 500,
+      targetWidth: 500,
+      saveToPhotoAlbum: false,
+      correctOrientation: true
+    };
+    
+
+    this.camera.getPicture(options).then((imageUri) => {
+      this.imgPath = imageUri;
+    });
   }
 
 }
