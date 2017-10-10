d4697d342ae2fed48e7f0f7a5cad92a78afdf194
Steve Michelotti <steve.michelotti@gmail.com>
2017-10-09 23:19:14 -0400

    added copy

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,17 +1,24 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Camera, CameraOptions, DestinationType, EncodingType, PictureSourceType } from '@ionic-native/camera';
+import { File } from '@ionic-native/file';
+import { FilePath } from '@ionic-native/file-path';
+
+declare var cordova: any;
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  private imgPath: string;
+  private imgSrc: string;
 
-  constructor(public navCtrl: NavController, private camera: Camera) {
-
-  }
+  constructor(
+    public navCtrl: NavController, 
+    private camera: Camera, 
+    //private transfer: Transfer, 
+    private file: File, 
+    private filePath: FilePath) { }
 
   takePicture() {
     let options: CameraOptions = {
@@ -28,12 +35,22 @@ export class HomePage {
     this.camera.getPicture(options).then((imagePath) => {
       //this.imgPath = imagePath;
       var currentName = imagePath.substr(imagePath.lastIndexOf('/') + 1);
-      var correctPath = imagePath.substr(0, imagePath.lastIndexOf('/') + 1);
+      var currentPath = imagePath.substr(0, imagePath.lastIndexOf('/') + 1);
       alert(`After get picture: ${imagePath}`)
       alert(`currentName: ${currentName}`); 
-      alert(`correctPath: ${correctPath}`);      
-           
+      alert(`currentPath: ${currentPath}`);      
+      //this.copyFileToLocalDir(currectPath, currentName, this.createFileName());
+      this.copyFileToLocalDir(currentPath, currentName);
     });
+  }
+
+  copyFileToLocalDir(currentPath, currentName) {
+    this.file.copyFile(currentPath, currentName, cordova.file.datadirectory, currentName).then(success => {
+      this.imgSrc = currentName;
+    }, error => {
+      alert('Error while storing file.');
+      //this.presentToast('Error while storing file.');
+    });;
   }
 
 }
