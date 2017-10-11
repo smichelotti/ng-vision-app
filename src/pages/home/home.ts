23da10ac26b497b9ed1186354a09c4e318465d2d
Steve Michelotti <steve.michelotti@gmail.com>
2017-10-10 20:51:21 -0400

    removed alerts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { LoadingController, NavController } from 'ionic-angular';
 import { Camera, CameraOptions, DestinationType, EncodingType, PictureSourceType } from '@ionic-native/camera';
 import { File } from '@ionic-native/file';
-import { FilePath } from '@ionic-native/file-path';
+//import { FilePath } from '@ionic-native/file-path';
 import { FileTransfer, FileUploadOptions } from '@ionic-native/file-transfer';
 
 declare var cordova: any;
@@ -20,69 +20,64 @@ export class HomePage {
     private camera: Camera, 
     private fileTransfer: FileTransfer, 
     private file: File, 
-    private filePath: FilePath) { }
+    //private filePath: FilePath,
+    public loadingCtrl: LoadingController) { }
 
   takePicture() {
     let options: CameraOptions = {
       destinationType: DestinationType.FILE_URL,
-      //destinationType: DestinationType.DATA_URL,
       sourceType: PictureSourceType.CAMERA,
       encodingType: EncodingType.PNG,
-      targetHeight: 500,
-      targetWidth: 500,
+      // targetHeight: 500,
+      // targetWidth: 500,
       saveToPhotoAlbum: false,
       correctOrientation: true
     };
     
-    // this.camera.getPicture(options).then((imgData) => {
-    //   this.base64Image = "data:image/jpeg;base64," + imgData;
-    // });
-
     this.camera.getPicture(options).then((imagePath) => {
-      this.imgSrc = imagePath;
-      
+      //this.imgSrc = imagePath;
       var currentName = imagePath.substr(imagePath.lastIndexOf('/') + 1);
       var currentPath = imagePath.substr(0, imagePath.lastIndexOf('/') + 1);
-      alert(`After get picture: ${imagePath}`)
-      alert(`currentName: ${currentName}`); 
-      alert(`currentPath: ${currentPath}`);      
-      //this.copyFileToLocalDir(currectPath, currentName, this.createFileName());
       this.copyFileToLocalDir(currentPath, currentName);
     });
   }
 
   copyFileToLocalDir(currentPath, currentName) {
-    //alert(`**inside copyFile func; dir: ${cordova.file.dataDirectory}`);
-    alert(`**inside copyFile func; cordova: ${cordova}`);
-    alert(`**inside copyFile func; cordova.file: ${cordova.file}`);
-    alert(`file.dataDir: ${this.file.dataDirectory}`);
     this.file.copyFile(currentPath, currentName, cordova.file.dataDirectory, currentName).then(success => {
       this.imgSrc = cordova.file.dataDirectory + currentName;
-      alert(`success: ${success}`);
-      alert(`imgSrc: ${this.imgSrc}`);
+      // alert(`success: ${success}`);
+      // alert(`imgSrc: ${this.imgSrc}`);
     }, error => {
-      alert('Error while storing file.');
+      //alert('Error while storing file.');
       //this.presentToast('Error while storing file.');
     });;
   }
 
   uploadImage() {
+    const loading = this.loadingCtrl.create({
+      content: 'Analyzing...',
+    });
+
     let options: FileUploadOptions = {
       mimeType: 'application/octet-stream',
       headers: {
         'Ocp-Apim-Subscription-Key': 'b867a02e0594487fb6cdcb52d9dd11bd'
       }
     };
+    const visionApiUrl = 'https://eastus.api.cognitive.microsoft.com/vision/v1.0/describe';
 
     let fileTransfer = this.fileTransfer.create();
 
-    fileTransfer.upload(this.imgSrc, 'https://eastus.api.cognitive.microsoft.com/vision/v1.0/describe', options).then(data => {
-      alert(`**success: ${data}`);
-      alert(data.response);
+    loading.present();
+    fileTransfer.upload(this.imgSrc, visionApiUrl, options).then(data => {
+      // alert(`**success: ${data}`);
+      // alert(data.response);
       let json = JSON.parse(data.response);
+      loading.dismissAll();
       alert(json.description.captions[0].text);
     }, err => {
       alert(`**error: ${err}`);
+      loading.dismissAll();
     });
   }
 
